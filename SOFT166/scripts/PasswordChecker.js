83e12e474406dbe20b134f7c19656c6c2a37f74c
Michael-T-C <44771731+Michael-T-C@users.noreply.github.com>
2020-01-02 15:03:09 +0000

    Add files via upload

--- SOFT166/scripts/PasswordChecker.js
+++ SOFT166/scripts/PasswordChecker.js
@@ -1,5 +1,38 @@
 // JavaScript Document
 
+var pass = document.getElementByID("cPassword")
+pass.addEventListener('keyup', function() {
+	checkPassword(pass.value) 
+})
+
 function checkPassword(){
-	alert("Testing button and checkPassword function")
+	var strengthBar = document.getElementById("strengthMeter")
+	var strength = 0;
+	if (password.match(/[a-zA-Z0-9][a-zA-Z0-9]+/)){
+		strength += 1
+	}
+	if (password.match(/[!£$%^&*@~?<>]+/)){
+		strength += 1
+	}
+	if (password.length > 7){
+		strength += 1
+	}
+	switch(strength){
+		   case 0:
+		   		strengthBar.value = 10;
+		   		break
+		   case 1:
+		   		strengthBar.value = 40;
+		   		break
+		   case 2:
+		   		strengthBar.value = 60;
+		   		break
+		   case 3:
+		   		strengthBar.value = 80;
+		   		break
+		   case 4:
+		   		strengthBar.value = 100;
+		   		break
+		   	
+		   }
 }
